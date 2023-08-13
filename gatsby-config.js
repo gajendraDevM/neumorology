ff504644c375e1559ac722e9de0d03ceb85d6240
gajendra <46343909+gajendrahassan@users.noreply.github.com>
2023-08-13 22:58:21 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `astology_website_2`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
